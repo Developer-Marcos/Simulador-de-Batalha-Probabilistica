792f9b95580d999d408493d752aef8002edb59e9
Marcos <marcosgab2006@gmail.com>
2025-05-09 21:28:27 -0300

    .

---
+++ combate_por_turno/principal.py
@@ -0,0 +1,45 @@
+from personagens import *
+from acoes import *
+from os import system 
+
+system("cls")
+
+explicacao()
+
+print("Deseja começar o jogo?")
+
+comecar = input("Responda (S/N): ").lower()
+while True:
+      if comecar in ('nao','n'):
+            print('Tudo bem, caso mude de idéia é só executar o arquivo novamente.')
+            sleep(1)
+            exit()
+      elif comecar not in ('sim', 's'):
+            print('Digite apenas sim ou não.')
+            comecar = input('Responda (S/N): ')
+      else:
+            break
+sleep(1)
+system("cls")
+print('Laplace está ansioso para enfrenta-lo.')
+print('Boa sorte.')
+sleep(3)
+
+start = True if comecar in ("sim", "s") else False
+if not start:
+      sair()
+
+turno = 0
+while start:
+      if laplace.vida > 0:
+            system("cls")
+            turno += 1
+            status()
+            print(f"     Turno: {turno}")
+            print("     --------")
+            escolher()
+      else:
+            system('cls')
+            print("     Laplave foi derrotado com sucesso!")
+            print("     O sistema agradeceu ao jogador.\n")
+            sair()
